395417056adc9bba9eacbccf713b845f93742eed
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2014-01-31 21:18:00 -0500

    I failed, rien compris

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 team2ex2
 ========
+
+## message de gableroux:
+
+Lancer les doctests:
+
+    python -m doctest -v main.py
+
+
+Pour executer:
+
+    python main.py code line
+
+devrait ecrire:
+
+    code
+    cone
+    lone
+    line
+
+ou
+
+    code
+    bode
+    bade
+    bake
+    bike
+    dike
+    dihe
+    dine
+    line
+
+ou autre et en ce moment, ca ecrit ca:
+
+    (4, 4996)
+    {'coda': 5, 'codo': 5, 'rode': 4, 'coue': 4, 'cone': 3, 'tode': 4, 'cole': 4, 'gode': 4, 'cede': 4, 'come': 4, 'node': 4, 'core': 4, 'cope': 4, 'coke': 4, 'cove': 4, 'lode': 3, 'wode': 4, 'cote': 4, 'coze': 4, 'bode': 4, 'dode': 4, 'mode': 4, 'cade': 4}
+    End
+
+`(4, 4996)` c'est la distance de l'heuristique (je crois)
+
+j'ai comme pas compris grand chose en fait, need coffee. :(
--- main.py
+++ main.py
@@ -2,20 +2,39 @@ import sys
 import os
 import itertools
 
+#g ya du shit dans le readme
 
-#Faire un graph oriente avec un arc seulement si un mot a 1 seulemeent de difeerence
-#Puis faire une recherche de plus court chemin dans le graphe
 
+# Faire un graph oriente avec un arc seulement si un mot a 1 seulemeent de difeerence
+# Puis faire une recherche de plus court chemin dans le graphe
+
+#g on ne peut passer que par les mots dans le dictionnaire pour faire les sauts
+#g (ca m'a pris trop de temps pour compredre ca, dsl. Martin a code comme un malade et pas moi) 
+
+# lengthword a laire de fonctioner
+#g wat? mais ca marche...
 def lengthWord(word1, word2, maximum=100000):
     """
 
     >>> lengthWord("cat","cot")
     1
 
+    >>> lengthWord("penis","prots")
+    3
+
+    >>> lengthWord("asd","asd")
+    0
+
     """
-    if word1 == word2: return 0
+    if word1 == word2:
+        return 0
     nombreDifference = 0
-    for char1,char2 in itertools.zip_longest(word1,word2, fillvalue=' '):
+    
+    #g Itertools.izip_longest dans la doc:
+    #g Make an iterator that aggregates elements from each of the iterables. 
+    #g If the iterables are of uneven length, missing values are filled-in with fillvalue.
+    #g Iteration continues until the longest iterable is exhausted.
+    for char1, char2 in itertools.izip_longest(word1, word2, fillvalue=' '):
         if char1 != char2:
             nombreDifference = nombreDifference + 1
             if nombreDifference == maximum:
@@ -23,49 +42,51 @@ def lengthWord(word1, word2, maximum=100000):
     return nombreDifference
 
 
-#make A*, lengthWord is the heuristic
+
+# make A*, lengthWord is the heuristic
+#g (heuristic = a decouvrir)
 def findDist(graph, word1, word2):
     s = {word1}
 
-    distcourante = { x:1 + lengthWord(x,word2)  for x in graph[word1]}
-    print(distcourante)
-    #check list filter and stuff 
-    
-    
-    
-    
+    #g pour x dans le graph, la entre x et word2
+    distance_courante = {x: 1 + lengthWord(x, word2) for x in graph[word1]}
+    print(distance_courante)
+    # check list filter and stuff 
+    #g (C'est a faire ou quoi xD  je crois que oui, fuu, je sais pas quoi faire ici... ca marche?)
 
-if __name__=="__main__":
 
-    sys.argv = ["","cat","dog"] #Pour tester 
-    
+
+if __name__ == "__main__":
+
+    # sys.argv = ["", "cat", "dog"]  # Pour tester
+
     if len(sys.argv) < 3:
-        print("Pas assez d'argument")
+        print("Pas assez d'arguments")
 
-    word1,word2 = sys.argv[1:3]
+    elif len(sys.argv) > 3:
+        print("trop d'arguments")
+
+    word1, word2 = sys.argv[1:3]
 
     if len(word1) != len(word2):
         print("Ne peut etre resolu")
-    
+
     with open("dictionnaire.txt") as f:
+        #g retrouve tous les mots du dictionnaire
         words = {x.strip().lower() for x in f}
 
         graph = {}
-        i=len(word1)
+        i = len(word1)
         wordsOfLengthI = list(filter(lambda x: len(x) == i, words))
         print(i, len(wordsOfLengthI))
         for w1 in wordsOfLengthI:
             for w2 in wordsOfLengthI:
-                if lengthWord(w1,w2,2)==1:
+                if lengthWord(w1, w2, 2) == 1:
                     if w1 in graph:
                         graph[w1].append(w2)
                     else:
                         graph[w1] = [w2]
-            
-                        
 
-        #print(graph)
-        findDist(graph,word1,word2)
+        # print(graph)
+        findDist(graph, word1, word2)
         print("End")
-        
-        
